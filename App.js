d6090ee08286d01dc57bdb30530435d10911ff7d
maiakauaa <kaua.cousillas03@gmail.com>
2021-10-05 12:01:07 -0300

    videoaula operacoes matematicas...

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+* @bainloko
+* DDM I
+* 05/10/2021
+*/
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from 'react';
+import {Text, TouchableOpacity, TextInput, View, StyleSheet, StatusBar} from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    valor1: 0.0,
+    valor2: 0.0,
+    resultado: 0.0
+  }
+
+  atualizaValor1 = (number) => {
+    this.setState({valor1:number})
+  }
+
+  atualizaValor2 = (number) => {
+    this.setState({valor2:number})
+  }
+
+  soma(){
+    this.state.resultado = parseFloat(this.state.valor1) + parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+
+  render(){
+    return(
+      <View style={meuEstilo.container}>
+        <StatusBar />
+        <Text>Operações Matemáticas...</Text>
+        <TextInput style={meuEstilo.inputExemplo} underlineColorAndroid="transparent" placeholder="Digite o primeiro valor" autoCapitalize="none" onChangeText={this.atualizaValor1} keyboardType="numeric" />
+        <TextInput style={meuEstilo.inputExemplo} underlineColorAndroid="transparent" placeholder="Digite o segundo valor" autoCapitalize="none" onChangeText={this.atualizaValor2} keyboardType="numeric" /> 
+        <TouchableOpacity onPress={()=>this.soma(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={meuEstilo.fonteExemplo}>Somar</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
+const meuEstilo = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+
+  inputExemplo: {
+    border: '1px solid black',
+    marginTop: 2
+  },
+
+  fonteExemplo: {
+    color: 'blue',
+    fontSize: 15
+  }
+})
