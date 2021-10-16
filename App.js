e9e7cd56cd61b5d1db530ed628a52bb23f33b90b
maiakauaa <kaua.cousillas03@gmail.com>
2021-10-15 21:16:59 -0300

    avaliacao 01 15/10/2021

--- App.js
+++ App.js
@@ -21,9 +21,24 @@ export default class App extends React.Component {
   atualizaValor2 = (number) => {
     this.setState({valor2:number})
   }
-
-  soma(){
+  
+  somar(){
     this.state.resultado = parseFloat(this.state.valor1) + parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+
+  subtrair(){
+    this.state.resultado = parseFloat(this.state.valor1) - parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+
+  multiplicar(){
+    this.state.resultado = parseFloat(this.state.valor1) * parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+  }
+
+  dividir(){
+    this.state.resultado = parseFloat(this.state.valor1) / parseFloat(this.state.valor2);
     alert(this.state.resultado);
   }
 
@@ -34,13 +49,30 @@ export default class App extends React.Component {
         <Text>Operações Matemáticas...</Text>
         <TextInput style={meuEstilo.inputExemplo} underlineColorAndroid="transparent" placeholder="Digite o primeiro valor" autoCapitalize="none" onChangeText={this.atualizaValor1} keyboardType="numeric" />
         <TextInput style={meuEstilo.inputExemplo} underlineColorAndroid="transparent" placeholder="Digite o segundo valor" autoCapitalize="none" onChangeText={this.atualizaValor2} keyboardType="numeric" /> 
-        <TouchableOpacity onPress={()=>this.soma(this.state.valor1, this.state.valor2, this.state.resultado)}>
+        <TouchableOpacity onPress={()=>this.somar(this.state.valor1, this.state.valor2, this.state.resultado)}>
           <Text style={meuEstilo.fonteExemplo}>Somar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.subtrair(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={meuEstilo.fonteExemplo}>Subtrair</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.multiplicar(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={meuEstilo.fonteExemplo}>Multiplicar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.dividir(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={meuEstilo.fonteExemplo}>Dividir</Text>
         </TouchableOpacity>
       </View>
     )
   }
 }
+
+/** 
+* como fazer as operações (funções) fora da classe?
+* como chamar os valores como argumentos, uma vez que estão tipados na classe?
+* como verificar erros na entrada de dados? (letras, caracteres especiais no lugar dos números...) - como inserir try catch ...
+* como colocar os botões um ao lado do outro? inline-block ou ...
+* como fazer uma interface de app de verdade? ideias behance ou aguardar próximas aulas
+*/
 
 const meuEstilo = StyleSheet.create({
   container: {
@@ -59,4 +91,4 @@ const meuEstilo = StyleSheet.create({
     color: 'blue',
     fontSize: 15
   }
-})
+});
